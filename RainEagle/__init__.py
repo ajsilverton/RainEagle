1cbeb5914bbc1b1723de0a996a7c2152af3d30c2
Aron Silverton <aron.j.silverton@gmail.com>
2017-03-04 15:47:53 -0600

    RainEagle-1 PEP8 Cleanup
    - Reformatted code to fix tab / space and indentation issues experienced upon clone and import to PyCharm
    - Fixed most obvious PEP8 issues
    
    No gaurantee that all issues were addressed or that code will run in current state. Additional issues will be fixed as found while working on other features.

--- RainEagle/__init__.py
+++ RainEagle/__init__.py
@@ -1,38 +1,31 @@
-"""Simple class for talking to The EAGLE gateway ( RFA-Z109 ) from rainforest automation
-
-"""
-
+"""Simple class for talking to The EAGLE gateway ( RFA-Z109 ) from rainforest automation"""
 
 import sys
-if sys.hexversion < 0x20703f0 :
+
+# Todo: Update version checking to check for 2.7.10, 3.4 3.6, if needed
+if sys.hexversion < 0x20703f0:
     sys.stderr.write("You need python 2.7 or later to run this script\n")
 
+# ToDo: Update author, credits, maintainer, version, etc.
 __revision__ = "$Id: 20140301 $"
 __version__ = "0.1.9"
 __author__ = 'Peter Shipley <peter.shipley@gmail.com>'
 __copyright__ = "Copyright (C) 2015 Peter Shipley"
 __license__ = "BSD"
 
-
 import EagleClass
 from EagleClass import Eagle, RainEagleResponseError, Eagle_cloud, Eagle_cgi
-from RainEagle.Eagle_util import to_epoch_1970, to_epoch_2000  
+from RainEagle.Eagle_util import to_epoch_1970, to_epoch_2000
 from RainEagle.Eagle_cloud import Eagle_cloud
 from RainEagle.Eagle_cgi import Eagle_cgi
 
-
-#from RainEagle.EagleClass import Eagle
+# from RainEagle.EagleClass import Eagle
 
 __all__ = ['Eagle', 'Eagle_cloud', 'Eagle_cgi', 'RainEagleResponseError', 'to_epoch_1970', 'to_epoch_2000']
 
-
-
 if __name__ == "__main__":
-    #import __main__
-    #print(__main__.__file___)
+    # import __main__
+    # print(__main__.__file___)
     print("RainEagle.__init__")
     print("syntax ok")
     exit(0)
-
-
-
